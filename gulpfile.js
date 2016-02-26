a88748ddf9b9fd0b653e2514b892a7d6a23637f0
Peter Tollnes Flem <peter.t.flem@gmail.com>
2016-02-26 17:51:56 +0000

    Working on the mobile landing page.

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,17 @@
 var gulp = require('gulp');
 var less = require('gulp-less');
 var watch = require('gulp-watch');
+var livereload = require('gulp-livereload');
 
 gulp.task('less', function() {
   return gulp.src('./design/stylesheets/less/main.less')
     .pipe(less())
-    .pipe(gulp.dest('./design/stylesheets/css/'))
+    .pipe(gulp.dest('./design/stylesheets/css/'));
+    //.pipe(livereload());
 });
 
 gulp.task('watch', function () {
+  //livereload.listen({ host: '192.168.0.1' });
   gulp.watch('./design/stylesheets/less/**/*.less', ['less']);
 });
 
